fba7847f383efece652fc90031c62b92f6338ff9
Sushmasri99 <42650865+Sushmasri99@users.noreply.github.com>
2019-02-01 17:23:52 -0600

    icp 2

---
+++ Python icp 2/list.py
@@ -0,0 +1,11 @@
+listOfNum = [23, 45, 21, 45,92, 2, 5, 11, 50, 1, 67,90]
+
+# print the List
+print("Initial List", listOfNum, sep='\n')
+
+print("Sorting the List in ascending Order")
+
+# Create a sorted copy of existing list
+newList = sorted(listOfNum)
+# print the List
+print("New List", newList, sep='\n')
